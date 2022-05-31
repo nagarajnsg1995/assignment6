b5cd78cffe41b6d43f7d8b1adefa2a6f49b7c53b
nagarajnsg1995 <104911894+nagarajnsg1995@users.noreply.github.com>
2022-05-31 19:14:37 +0530

    Update stopwatch.java

--- stopwatch.java
+++ stopwatch.java
@@ -1,27 +1,18 @@
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Stopwatch {
-    static int interval;
-    static Timer timer;
 
-        public static void main(String[] args) {
-
-
-            int delay = 1000;
-            int period = 1000;
-            timer = new Timer();
-            interval =10;
-            timer.scheduleAtFixedRate(new TimerTask() {
-                public void run() {
-                    System.out.println(setInterval());
-                }
-            }, delay, period);
-        }
-
-        private static final int setInterval(){
-            if( interval== 1) timer.cancel();
-            return --interval;
+    public static void main(String[] args) throws InterruptedException
+        {
+            long starttime = System.currentTimeMillis();
+            for (int i=0; i<10; i++)
+            {
+                System.out.println(i);
+                Thread.sleep(500);
+            }
+            long endtime = System.currentTimeMillis();
+            long elapsetime = endtime - starttime;
+            System.out.println("execution time " +elapsetime);
         }
     }
+
+
 
